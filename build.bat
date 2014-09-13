da34a9d73aa66bfa137361bb6d0621abb4ca7c65
dabarrett42@students.tntech.edu <dabarrett42@students.tntech.edu>
2014-09-13 14:04:36 -0500

    File cleanup

--- build.bat
+++ build.bat
@@ -4,22 +4,13 @@ cls
 set DRIVE_LETTER=%1:
 set PATH=%DRIVE_LETTER%\MinGW\bin;%DRIVE_LETTER%\MinGW\msys\1.0\bin;c:\Windows;c:\Windows\system32
 
-g++ -c String.cpp
-g++ -c Tokens.cpp
-g++ -c ReadFile.cpp
-g++ -c WriteFile.cpp
-g++ -c Keyboard.cpp
-g++ -c Random.cpp
-g++ -c Matrix.cpp
-g++ -c Poly.cpp
-g++ -c CD.cpp
-g++ -c Song.cpp
-g++ -c Integer.cpp
-g++ -c Double.cpp
-g++ -c HighPerformanceCounter.cpp
-g++ -c Permutation.cpp
+g++ -I.\CSC2110 -c Password.cpp
+g++ -I.\CSC2110 -c PasswordDriver.cpp
 
-ar -r libCSC2110.a *.o
+g++ -L.\CSC2110 -o  Password.exe PasswordDriver.o Password.o -lCSC2110
+Password <input.txt
+
+
 
 
 
